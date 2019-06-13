03d180f1aa5010a7304de248958dcbc70e7c74ba
Lucas Julio Alves Da Costa <lucasjulio@brq.com>
2019-06-13 19:10:53 -0300

    angular material adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [
     StatusBar,
--- src/app/modules/shared-modules.module.ts
+++ src/app/modules/shared-modules.module.ts
@@ -5,26 +5,29 @@ import { MaskDirective } from '../directives/mask.directive';
 import { PaginaFooterComponent } from '../common/components/pagina-footer/pagina-footer.component';
 import { IonicModule } from '@ionic/angular';
 import { CarregandoComponent } from '../common/components/carregando/carregando.component';
-import { MatExpansionPanel  } from '@angular/material';
+import { MatExpansionPanel, MatExpansionModule  } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortalModule } from '@angular/cdk/portal';
 
 @NgModule({
   declarations: [
     CpfValidator,
     MaskDirective,
     PaginaFooterComponent,
-    CarregandoComponent,
-    MatExpansionPanel
+    CarregandoComponent
   ],
   imports: [
     CommonModule,
     IonicModule,
+    PortalModule,
+    MatExpansionModule
   ],
   exports: [
     CpfValidator,
     MaskDirective,
     PaginaFooterComponent,
     CarregandoComponent,
-    MatExpansionPanel
+    MatExpansionModule
   ]
 })
 export class SharedModules { }

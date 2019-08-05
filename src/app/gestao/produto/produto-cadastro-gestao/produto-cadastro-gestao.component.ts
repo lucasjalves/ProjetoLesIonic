a5f67a6edd798cf871ab270ee51fc5feaeeef128
Lucas Julio Alves da costa <lucasjulioalves@gmail.com>
2019-08-05 00:07:05 -0300

    inserção de pedido

--- src/app/gestao/produto/produto-cadastro-gestao/produto-cadastro-gestao.component.ts
+++ src/app/gestao/produto/produto-cadastro-gestao/produto-cadastro-gestao.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from 'src/app/produto/model/produto.model';
 import { Router } from '@angular/router';
+import { ProdutoService } from 'src/app/produto/service/produto.service';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { ModalHelper } from 'src/app/common/modal.helper';
+import { Resultado } from 'src/app/common/resultado.model';
 
 @Component({
   selector: 'app-produto-cadastro-gestao',
@@ -11,14 +15,40 @@ export class ProdutoCadastroGestaoComponent implements OnInit {
 
   public funcao: () => void;
   public produto = new Produto();
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private produtoService: ProdutoService,
+              private loadingController: LoadingController,
+              private alertController: AlertController,
+              private modalHelper: ModalHelper) {
     this.funcao = this.cadastrar.bind(this);
   }
 
   ngOnInit() {}
 
-  cadastrar() {
+  async cadastrar() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando...'
+    });
 
+    loading.present();
+
+    const json = this.produto.serialize();
+    const p = this.produto.deserialize(json);
+    p.precoCompra = p.precoCompra.replace(/\./g, 'l').replace(/\,/g, '.').replace(/\l/g, ',').replace('R$', '');
+    p.precoVenda = p.precoVenda.replace(/\./g, 'l').replace(/\,/g, '.').replace(/\l/g, ',').replace('R$', '');
+
+    p.ativo = true;
+    this.produtoService.salvarProduto(p).subscribe(res => {
+      const resultado = new Resultado(this.produto).deserialize(res);
+      loading.dismiss();
+      this.modalHelper.mostrarModal(this.alertController, 'Sucesso', 'Produto cadastrado com sucesso', resultado,
+      () => {
+        this.router.navigateByUrl('/gestao/produtos');
+      }).then(modal => modal.present());
+    }, err => {
+      loading.dismiss();
+      this.modalHelper.mostrarModal(this.alertController).then(modal => modal.present());
+    });
   }
 
 }
